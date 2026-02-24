b43d86fff82bdac107a11fbbee5cdc22cc597c46
Diego (罗杰斯) <noreply@labrouste.net>
2026-02-24 22:43:25 +0100

    Update configure.py

--- configure.py
+++ configure.py
@@ -2,5 +2,10 @@ import subprocess
 
 
 subprocess.run(["git", "config", "--global", "user.name", "Diego (罗杰斯)"], check=True)
-subprocess.run(["git", "config", "--global", "user.email", "noreply@labrouste.net"], check=True)
+subprocess.run(["git", "config", "--global", "user.email", "diego@labrouste.net"], check=True)
 print("Configured git user information.")
+
+subprocess.run(["git", "config", "--global", "push.autoSetupRemote", "true"], check=True)
+subprocess.run(["git", "config", "--global", "pull.rebase", "true"], check=True)
+subprocess.run(["git", "config", "--global", "stash.autoStash", "true"], check=True)
+print("Configured git settings.")
